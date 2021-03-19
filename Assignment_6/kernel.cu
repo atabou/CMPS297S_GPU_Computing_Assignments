132b813811f1ad185d4d1263b9f5343b03e93908
Andre Tabourian <andretabourian@hotmail.com>
2021-03-19 17:13:00 +0200

    First Iteration

--- Assignment_6/kernel.cu
+++ Assignment_6/kernel.cu
@@ -8,24 +8,68 @@ __global__ void scan_kernel(float* input, float* output, float* partialSums, uns
 
     // TODO
 
+    unsigned int segment = 2*blockIdx.x*blockDim.x;
 
+    __shared__ float buffer_s[2*BLOCK_DIM];
 
+    buffer_s[threadIdx.x] = input[segment+threadIdx.x];
+    buffer_s[threadIdx.x + BLOCK_DIM] = input[segment + threadIdx.x + BLOCK_DIM];
 
+    __syncthreads();
+   
+    // Reduction
 
+    for( int stride=1; stride<=BLOCK_DIM; stride*=2) {
 
+        unsigned int i = 2*(threadIdx.x+1)*stride - 1;
 
+        if( i < 2*BLOCK_DIM ) {
+            buffer_s[i] += buffer_s[i-stride];
+        }
 
+        __syncthreads();
 
+    }
 
+    if( threadIdx.x == 0 ) {
 
+        partialSums[blockIdx.x] = buffer_s[2*BLOCK_DIM - 1];
+        buffer_s[2*BLOCK_DIM - 1] = 0.0f;
 
+    }
 
+    // Post-Reduction
 
+    for(unsigned int stride=BLOCK_DIM; i>=1; i/=2) {
 
+        int i = 2*(threadIdx.x + 1)*stride - 1;
 
+        if( i+stride < 2*BLOCK_DIM ) {
+            float temp = buffer_s[i+stride];
+            buffer_s[i+stride] += buffer_s[i];
+            buffer_s[i] = temp;
+        }
 
+        __syncthreads();
 
+    }
 
+    // Post-Reduction
+
+    // for( unsigned int stride=BLOCK_DIM/2; stride>=1; stride /= 2 ) {
+
+    //     unsigned int i = 2*(threadIdx.x + 1)*stride - 1;
+        
+    //     if( i + stride < 2*BLOCK_DIM ) {
+    //         buffer_s[i+stride] += buffer_s[i];
+    //     }
+
+    //     __syncthreads();
+
+    // }
+
+    output[segment+threadIdx.x] = buffer_s[threadIdx.x];
+    output[segment+threadIdx.x+BLOCK_DIM] = buffer_s[threadIdx.x+BLOCK_DIM];
 
 }
 
@@ -33,9 +77,12 @@ __global__ void add_kernel(float* output, float* partialSums, unsigned int N) {
 
     // TODO
 
-
-
-
+    // Location of excluded element is at index i
+    unsigned int segment = 2*blockIdx.x*blockDim.x;
+    if(blockIdx.x > 0) {
+        output[segment + threadIdx.x] += partialSums[blockIdx.x - 1];
+        output[segment + threadIdx.x + BLOCK_DIM] += partialSums[blockIdx.x - 1];
+    }
 
 
 
